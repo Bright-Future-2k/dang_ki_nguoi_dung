7493b2cc5d0f743bf15ea13c8739fde5e741fed2
luongtuananh1911 <avatarboy763@gmail.com>
2019-09-12 09:24:39 +0700

    bai tap

---
+++ example.php
@@ -0,0 +1,19 @@
+<?php
+class customException extends Exception{
+    public function errorMessage()
+    {
+        $errorMsg = 'Error on line ' . $this->getLine() . ' in ' . $this->getFile()
+            . ': <b>' . $this->getMessage() . '</b> is not a valid E-Mail address';
+        return $errorMsg;
+    }
+}
+$email = "abc@gmail.com";
+try{
+    if (filter_var($email,FILTER_VALIDATE_EMAIL) === false) {
+        throw new customException($email);
+    }
+}
+catch (customException $e){
+    echo $e->errorMessage();
+}
+?>
